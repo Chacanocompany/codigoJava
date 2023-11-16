d22abe75091b5fc3e14ba2c5b7c0e06b49509083
Christopher <deep.paice@gmail.com>
2023-11-15 23:05:00 -0300

    mas codigo de ejercicios

--- Main.java
+++ Main.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hola CTMRE !!!");
 
+        Scanner teclado = new Scanner(System.in);
+
+
+        /*
+        Integer edad = teclado.nextInt();
+        Integer edad = teclado.nextInt();
+        Integer edadPerro = edad * 7;
+        System.out.println("Su perro en edad humana tendria: " + edadPerro);
+
+
+        System.out.println("Ingrese una distancia en millas: ");
+        double millas = teclado.nextDouble();
+        double kilometros = millas * 1.60934;
+        System.out.println("La distancia en Kilometros es: " + kilometros);
+
+
+        System.out.println("Ingrese precio original del producto: ");
+        float precioOriginal = teclado.nextFloat();
+        System.out.println("Ingrese porcentaje del descuento: ");
+        float descuento = teclado.nextFloat();
+        float precioFinal = precioOriginal - (precioOriginal * descuento / 100);
+        System.out.println("El precio final del producto es de: " + precioFinal);
+
+
+        System.out.println("Ingrese monto total de la cuenta: ");
+        float totalCuenta = teclado.nextFloat();
+        System.out.println("Ingrese porcentaje de propina: ");
+        float porcentajePropina = teclado.nextFloat();
+        float propina = totalCuenta * (porcentajePropina / 100);
+        System.out.println("La propina será de: " + propina);
+        */
+
+        Integer numeroAleatorio = (int) (Math.random() * 10);
+        System.out.println("Adivine un numero: ");
+        int ingresoUsuario = teclado.nextInt();
+        if(ingresoUsuario == numeroAleatorio){
+            System.out.println("Acerto!");
+        }else{
+            System.out.println("Incorrecto, el numero era: " + numeroAleatorio);
+        }
     }
 }
